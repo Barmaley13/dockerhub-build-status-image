9d71cf280af57ef6b86b288e848f9ae3743aaac1
Unknown <kbelyayev@gmail.com>
2018-11-19 15:24:10 -0700

    Adding error handler

--- status.js
+++ status.js
@@ -41,10 +41,26 @@ function request_from_server(server) {
           console.log("Response from " + url);
           console.log(result);
           status_arrived(url, result);
+      },
+      error: function (jqXHR, exception) {
+          var msg = '';
+          if (jqXHR.status === 0) {
+              msg = 'Not connect.\n Verify Network.';
+          } else if (jqXHR.status == 404) {
+              msg = 'Requested page not found. [404]';
+          } else if (jqXHR.status == 500) {
+              msg = 'Internal Server Error [500].';
+          } else if (exception === 'parsererror') {
+              msg = 'Requested JSON parse failed.';
+          } else if (exception === 'timeout') {
+              msg = 'Time out error.';
+          } else if (exception === 'abort') {
+              msg = 'Ajax request aborted.';
+          } else {
+              msg = 'Uncaught Error.\n' + jqXHR.responseText;
+          }
+          console.log(msg);
       }
-      // error: function () {
-      //     console.log("error");
-      // }
   });
 
   // var url = server + "/build/" + parameters.organization + "/" + parameters.repository;
